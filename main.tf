bc66ed1b07185536d1bba6c7f24a0020f88c19b4
Julian Nonino <noninojulian@gmail.com>
2020-04-30 14:08:18 +0100

    Adding more outputs to module

--- main.tf
+++ main.tf
@@ -22,4 +22,3 @@ resource "aws_cloudwatch_log_stream" "log_stream" {
   name           = var.logs_path
   log_group_name = aws_cloudwatch_log_group.log_group.name
 }
-
--- outputs.tf
+++ outputs.tf
@@ -9,6 +9,16 @@ output "logs_path" {
 # ---------------------------------------------------------------------------------------------------------------------
 # AWS CLOUDWATCH LOG GROUP
 # ---------------------------------------------------------------------------------------------------------------------
+output "log_group_name" {
+  description = "The name of the log group."
+  value = aws_cloudwatch_log_group.log_group.name
+}
+
+output "log_group_retention_in_days" {
+  description = "Specifies the number of days log events are retained"
+  value = aws_cloudwatch_log_group.log_group.retention_in_days
+}
+
 output "log_group_arn" {
   description = "The Amazon Resource Name (ARN) specifying the log group."
   value       = aws_cloudwatch_log_group.log_group.arn
@@ -17,6 +27,16 @@ output "log_group_arn" {
 # ---------------------------------------------------------------------------------------------------------------------
 # AWS CLOUDWATCH LOG STREAM
 # ---------------------------------------------------------------------------------------------------------------------
+output "log_stream_name" {
+  description = "The name of the log stream."
+  value =  aws_cloudwatch_log_stream.log_stream.name
+}
+
+output "log_stream_log_group_name" {
+  description = "The name of the log group under which the log stream was created"
+  value = aws_cloudwatch_log_stream.log_stream.log_group_name
+}
+
 output "log_stream_arn" {
   description = "The Amazon Resource Name (ARN) specifying the log stream."
   value       = aws_cloudwatch_log_stream.log_stream.arn
