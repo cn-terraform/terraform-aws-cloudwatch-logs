46b438f640daf1a6fb05e4cd16692d56952b4d2b
Julian Nonino <juliannonino@hollandandbarrett.com>
2022-06-23 12:51:35 +0100

    Test localstack in pipeline

--- examples/encrypted-create-key/mock_provider.tf
+++ examples/encrypted-create-key/mock_provider.tf
@@ -16,4 +16,26 @@ provider "aws" {
   s3_use_path_style           = true
   access_key                  = "mock_access_key"
   secret_key                  = "mock_secret_key"
+
+  endpoints {
+    apigateway     = "http://localhost:4566"
+    cloudformation = "http://localhost:4566"
+    cloudwatch     = "http://localhost:4566"
+    dynamodb       = "http://localhost:4566"
+    es             = "http://localhost:4566"
+    firehose       = "http://localhost:4566"
+    iam            = "http://localhost:4566"
+    kinesis        = "http://localhost:4566"
+    lambda         = "http://localhost:4566"
+    route53        = "http://localhost:4566"
+    redshift       = "http://localhost:4566"
+    s3             = "http://localhost:4566"
+    secretsmanager = "http://localhost:4566"
+    ses            = "http://localhost:4566"
+    sns            = "http://localhost:4566"
+    sqs            = "http://localhost:4566"
+    ssm            = "http://localhost:4566"
+    stepfunctions  = "http://localhost:4566"
+    sts            = "http://localhost:4566"
+  }
 }
