db1b390926775666d3e363629169922fa3f8f3b4
Julian Nonino <noninojulian@gmail.com>
2022-06-24 00:13:46 +0100

    Trying to fix tests

--- examples/encrypted-custom-key/main.tf
+++ examples/encrypted-custom-key/main.tf
@@ -1,8 +1,14 @@
+resource "aws_kms_key" "custom_key" {
+  description             = "Custom key to test module"
+  deletion_window_in_days = 10
+  enable_key_rotation     = true
+}
+
 module "logs" {
   source = "../../"
 
   create_kms_key              = false
-  log_group_kms_key_id        = "01234567-89ab-cdef-0123-456789abcdef"
+  log_group_kms_key_id        = aws_kms_key.custom_key.key_id
   log_group_retention_in_days = 30
   logs_path                   = "test-path"
 }
